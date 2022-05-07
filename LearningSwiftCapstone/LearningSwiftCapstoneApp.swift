57e079b7ef9f40ece4a039f6bba89a90946d77cd
Tracy Estes <tracyestes7@gmail.com>
2022-05-07 10:49:02 -0500

    Initial Commit

---
+++ LearningSwiftCapstone/LearningSwiftCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftCapstoneApp.swift
+//  LearningSwiftCapstone
+//
+//  Created by Tracy Estes on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
