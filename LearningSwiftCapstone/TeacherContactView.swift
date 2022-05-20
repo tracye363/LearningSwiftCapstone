e2eb0d4b2f1425a83fba5e46316aae9571e12d44
Julian Bautista <julianbautista@icloud.com>
2022-05-20 18:57:37 -0500

    Teacher Schedule Data complete. Still needs styling

--- LearningSwiftCapstone/TeacherContactView.swift
+++ LearningSwiftCapstone/TeacherContactView.swift
@@ -12,6 +12,70 @@ struct TeacherContactView: View {
         ZStack {
             Color.mint
                 .ignoresSafeArea()
+            VStack {
+                VStack {
+                    HStack {
+                        Text("Name: ")
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Text("Email: ")
+                        Spacer()
+                    }
+                }
+                Spacer()
+                Text("Schedule")
+                    .font(.title)
+                VStack {
+                    Spacer()
+                    HStack {
+                        VStack {
+                            Text("Period")
+                                .font(.title2)
+                            Text("1st")
+                            Text("2nd")
+                            Text("3rd")
+                            Text("4th")
+                            Text("5th")
+                            Text("6th")
+                            Text("7th")
+                        }
+                        Spacer()
+                        Divider()
+                        Spacer()
+                        VStack {
+                            Text("Location")
+                                .font(.title2)
+                            Text("200")
+                            Text("200")
+                            Text("205")
+                            Text("153")
+                            Text("204")
+                            Text("202")
+                            Text("204")
+                        }
+                        Spacer()
+                        Divider()
+                        Spacer()
+                        VStack {
+                            Text("Subject")
+                                .font(.title2)
+                            Text("algebra")
+                            Text("algebra")
+                            Text("ecs")
+                            Text("geo")
+                            Text("plan")
+                            Text("algebra")
+                            Text("plan")
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            .padding(25.0)
         }.navigationBarTitle("Teacher Contact")
     }
 }
